2d1c3aba6df49e1a61eb78b4045d717885b0f74b
hoangkhoachau <21120481@student.hcmus.edu.vn>
2022-11-24 11:27:01 +0700

    init

--- socket.cpp
+++ socket.cpp
@@ -1,53 +1,68 @@
 #include "socket.h"
+#include <filesystem>
 #include <fstream>
+#include <future>
 #include <iostream>
+#include <queue>
 #include <sstream>
 #include <string>
+#include <vector>
 using namespace std;
 
-void writeString(string &buffer, int bufferSize, ostream &fout,
-                        bool &passedHeader, bool &done, int &cached,
-                        int &contentLength, int &contentDownloaded) {
-        int headerEnding = buffer.find("\r\n\r\n");
-        fout.write(&buffer[0], headerEnding);
-        done = true;
+enum class linkType { folder, file, unknown };
+
+vector<string> getLinksOfFolder(string &url, string &html) {
+    int lastFound = 0, quote = 0;
+    vector<string> links;
+    while (lastFound != -1) {
+        lastFound = html.find("href=\"", lastFound + 6);
+        quote = html.find('"', lastFound + 6);
+        if (lastFound != -1 && isalnum(html[lastFound + 6]))
+            links.push_back(url +
+                            html.substr(lastFound + 6, quote - lastFound - 6));
+    }
+    return links;
 }
 
-void writeContentLength(string &buffer, int bufferSize, ostream &fout,
-                        bool &passedHeader, bool &done, int &cached,
-                        int &contentLength, int &contentDownloaded) {
-    if (!passedHeader) {
-        int headerEnding = buffer.find("\r\n\r\n");
-        fout.write(&buffer[headerEnding + 4], bufferSize - headerEnding - 4);
-        contentDownloaded -= headerEnding + 4;
-        passedHeader = true;
-    } else {
-        if (contentDownloaded == contentLength)
-            done = true;
-        fout.write(&buffer[0], cached);
+addrinfo *resolveDomain(string domain) {
+    addrinfo hints;
+    addrinfo *res;
+    /* cout << "Resolving " << domain << " ... "; */
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    if (getaddrinfo(&domain[0], "http", &hints, &res)) {
+        /* cout << "failed\n"; */
+        return NULL;
     }
+    return res;
 }
 
-void writeChunked(string &buffer, int bufferSize, ostream &fout,
-                  bool &passedHeader, bool &done, int &cached,
-                  int &contentLength, int &contentDownloaded) {
-    int writePos = 0, writeLength = 0;
-    if (!passedHeader) {
-        writePos = buffer.find("\r\n\r\n") + 4;
-        writeLength = bufferSize - writePos;
-        passedHeader = true;
-    }
-    while (writePos < cached) {
+void writeString(string &buffer, int start, int end, ostream &fout, bool &done,
+                 int &contentDownloaded, int &contentLength) {
+    fout.write(&buffer[start], end - start);
+}
+
+void writeContentLength(string &buffer, int start, int end, ostream &fout,
+                        bool &done, int &contentDownloaded,
+                        int &contentLength) {
+    fout.write(&buffer[start], end - start);
+}
+
+void writeChunked(string &buffer, int start, int end, ostream &fout, bool &done,
+                  int &contentDownloaded, int &contentLength) {
+    int writePos = start, writeLength = 0;
+    while (writePos < end) {
         if (!contentLength) {
             char *p;
             contentLength = strtol(&buffer[writePos], &p, 16);
-            if (!contentLength) {
-                done = true;
+            done = !contentLength;
+            if (done)
                 return;
-            }
+            contentDownloaded += contentLength;
             writePos = buffer.find("\r\n", p - &buffer[0]) + 2;
         }
-        writeLength = min(contentLength, cached - writePos);
+        writeLength = min(contentLength, end - writePos);
         fout.write(&buffer[writePos], writeLength);
         writePos += writeLength;
         contentLength -= writeLength;
@@ -55,48 +70,65 @@ void writeChunked(string &buffer, int bufferSize, ostream &fout,
 }
 
 string getRequest(string &domain, string &filePath, string &fileName,
-                  string method, bool keepAlive) {
+                  string method, bool keepAlive, linkType type) {
     string requestMessage = " HTTP/1.1\r\nConnection: ";
-    requestMessage.insert(0, method + " " + filePath + "/" + fileName);
+    if (type == linkType::folder)
+        requestMessage.insert(0, "/");
+    if (type == linkType::unknown && filePath.empty())
+        filePath = "/";
+    requestMessage.insert(0, method + " " + filePath+fileName);
     requestMessage += keepAlive ? "keep-alive\r\n" : "close\r\n";
-    /* requestMessage += "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X "
-     */
-    /* "10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) " */
-    /* "Version/16.1 Safari/605.1.15\r\n"; */
     requestMessage += "Host: " + domain + "\r\n\r\n";
     return requestMessage;
 }
 
 void addressProcess(string addressString, string &domain, string &filePath,
-                    string &fileName) {
+                    string &fileName, linkType &type) {
     filePath = "", fileName = "";
     if (!addressString.find("http://"))
         addressString.erase(0, 7);
     int firstSlash = addressString.find_first_of('/'),
         lastSlash = addressString.find_last_of('/');
+    if (firstSlash != -1) {
+        if (addressString.back() == '/') {
+            type = linkType::folder;
+            addressString.pop_back();
+            lastSlash = addressString.find_last_of('/');
+        } else {
+            if (addressString.find('.', lastSlash) != -1)
+                type = linkType::file;
+            else
+                type = linkType::unknown;
+        }
+        if (lastSlash != -1) {
+            filePath =
+                addressString.substr(firstSlash, lastSlash - firstSlash + 1);
+            fileName = addressString.substr(lastSlash + 1);
+        }
+    } else
+        type = linkType::unknown;
     domain = addressString.substr(0, firstSlash);
-    if (firstSlash != -1)
-        filePath = addressString.substr(
-            firstSlash, (lastSlash != -1 && lastSlash != firstSlash)
-                            ? (lastSlash - firstSlash)
-                            : -1);
-    if (lastSlash != -1 && lastSlash != addressString.length() - 1)
-        fileName = addressString.substr(lastSlash + 1);
 }
 
 class Socket {
   public:
     int timeout, portNum, bufferSize;
+    bool connected;
+    string domain;
 
     Socket() {
 #ifdef WIN32
         WSADATA d;
         WSAStartup(MAKEWORD(2, 2), &d);
 #endif
-        sockfd = socket(AF_INET6, SOCK_STREAM, 0);
         timeout = 5000;
-        bufferSize = 1024 * 1024;
+        bufferSize = 2 * 1024 * 1024;
         buffer = string(bufferSize, 0);
+        connected = false;
+    }
+    Socket(string domain) {
+        *this = Socket();
+        this->domain = domain;
     }
 
     ~Socket() {
@@ -108,19 +140,7 @@ class Socket {
 #endif
     }
 
-    addrinfo *resolveDomain(string &domain) {
-        addrinfo hints;
-        addrinfo *servinfo;
-        memset(&hints, 0, sizeof(hints));
-        hints.ai_family = AF_UNSPEC;
-        hints.ai_socktype = SOCK_STREAM;
-        if (getaddrinfo(domain.c_str(), "http", &hints, &servinfo))
-            return NULL;
-        return servinfo;
-    }
-
     bool connect(addrinfo *addr) {
-        bool status = false;
 #ifdef WIN32
         u_long nonBlock = 1;
         ioctlsocket(sockfd, FIONBIO, nonBlock);
@@ -128,27 +148,31 @@ class Socket {
         fcntl(sockfd, F_SETFL, O_NONBLOCK);
 #endif
         pollfd pfds[1];
-        pfds[0].fd = sockfd;
         pfds[0].events = POLLOUT;
-        char ipstr[200]={0};
-        for (addrinfo *p = addr; p && !status; p = p->ai_next) {
-        inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
-            cout << "\n" << ipstr << "\n";
-            status = !::connect(sockfd, addr->ai_addr, addr->ai_addrlen);
+        for (addrinfo *p = addr; p && !connected; p = p->ai_next) {
+            sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+            pfds[0].fd = sockfd;
+            /* cout << "Connecting... "; */
+            connected = !::connect(sockfd, p->ai_addr, p->ai_addrlen);
             int numEvent = poll(pfds, 1, timeout);
-            if (!numEvent) {
-                continue;
-            } else if (pfds[0].revents & POLLIN) {
-                status = true;
+            if (numEvent && pfds[0].revents & POLLOUT) {
+                /* cout << "connected\n"; */
+                connected = true;
+                break;
             }
+            /* cout << "failed\n"; */
         }
         freeaddrinfo(addr);
-        return status;
+        return connected;
     }
 
-    bool connect(string domain) { return connect(resolveDomain(domain)); }
+    bool connect(string domain) {
+        addrinfo *t = (resolveDomain(domain));
+        this->domain = domain;
+        return connect(t);
+    }
 
-    bool sendRequest(string request) {
+    bool sendRequest(string &request) {
         bool status = false;
         pollfd pfds[1];
         pfds[0].fd = sockfd;
@@ -161,63 +185,148 @@ class Socket {
         return status;
     }
 
-
-    bool readResponse(ostream& fout,void (*writeFunc)(string &, int, ostream &, bool &,
-                                        bool &, int &, int &, int &)) {
-        bool done = false, passedHeader = false;
-        int contentLength = 0, contentDownloaded = 0, cached = 0, n = 1;
+    bool readResponse(ostream &fout) {
+        bool done = false, passedHeader = false, chunked = false;
+        int contentLength = 0, contentDownloaded = 0, cached = 0, n, start = 0;
         pollfd pfds[1];
         pfds[0].fd = sockfd;
         pfds[0].events = POLLIN;
         while (!done) {
             if (bufferSize - cached > 0) {
                 int numEvent = poll(pfds, 1, timeout);
-                if (numEvent && pfds[0].revents & POLLIN)
+                if (numEvent && pfds[0].revents & POLLIN) {
                     n = recv(sockfd, &buffer[cached], bufferSize - cached, 0);
-                else
+                    cached += n;
+                    /* cout << n << '\n'; */
+                } else
                     return false;
-                cached += n;
                 contentDownloaded += n;
-                if (done) {
+                if (!passedHeader) {
+                    int headerEnding =
+                        buffer.find("\r\n\r\n", max(cached - 4 - n, 0));
+                    if (headerEnding != -1) {
+                        chunked = buffer.find("chunked") != -1;
+                        if (!chunked) {
+                            int contentLengthPos =
+                                buffer.find("Content-Length: ") + 16;
+                            contentLength =
+                                strtol(&buffer[contentLengthPos], 0, 10);
+                        }
+                        passedHeader = true;
+                        start = headerEnding + 4;
+                        contentDownloaded -= start;
+                    }
+                }
+                if (chunked) {
+                    if (buffer[cached - 1] == '\n' &&
+                        buffer[cached - 2] == '\r' &&
+                        buffer[cached - 3] == '\n' &&
+                        buffer[cached - 4] == '\r')
+                        done = true;
+                } else {
+                    if (contentDownloaded >= contentLength)
+                        done = true;
+                }
+                if (done || n == 0) {
                     goto writeLast;
                 }
             } else {
             writeLast:
-                writeFunc(buffer, bufferSize, fout, passedHeader, done, cached,
-                          contentLength, contentDownloaded);
-                cached = 0;
+                if (chunked)
+                    writeChunked(buffer, start, cached, fout, done,
+                                 contentDownloaded, contentLength);
+                else
+                    writeContentLength(buffer, start, cached, fout, done,
+                                       contentDownloaded, contentLength);
+                start = 0, cached = 0;
             }
         }
         return true;
     }
 
-    bool download(string url){
-        bool status=false;
-        string domain, filePath, addressString, fileName;
-        addressProcess(url,domain,filePath,fileName);
-        ostringstream strstrm;
-        ofstream fout(domain + "_" + (fileName.length() ? fileName : "index.html"));
-        sendRequest(getRequest(domain, filePath, fileName, "HEAD", 1));
-        readResponse(strstrm,writeString);
-        string header=strstrm.str();
-        sendRequest(getRequest(domain,filePath,fileName,"GET",1));
-        if (header.find("chunked")){
-            readResponse(fout,writeChunked);
+    bool download(string &url, string path ) {
+        bool closed;
+        string filePath, addressString, fileName, request, html;
+        ofstream fout;
+        linkType type;
+        if (!filesystem::exists(path))
+            filesystem::create_directory(path);
+        addressProcess(url, domain, filePath, fileName, type);
+        if (!this->connected)
+            connect(domain);
+        closed = downloadQueue.size() == 1 && type != linkType::folder;
+        cout << fileName << ' ' << filePath << '\n';
+        request = getRequest(domain, filePath, fileName, "GET", !closed, type);
+        /* cout << request << '\n'; */
+        if (!sendRequest(request))
+            return false;
+        switch (type) {
+        case linkType::file:
+            fout.open(path + ((path=="./")?(domain + "_"):"") + fileName, ios::binary);
+            break;
+        case linkType::folder:
+        case linkType::unknown:
+            if (fileName.empty())
+                fileName = "index";
+            fout.open(path + ((path=="./")?(domain + "_"):"") + fileName + ".html", ios::binary);
         }
-        else{
-            readResponse(fout,writeContentLength);
+        if (!readResponse(fout))
+            return false;
+        fout.close();
+        if (type == linkType::folder) {
+            ifstream folderHtml(path + domain + "_" + fileName + ".html");
+            ostringstream ss;
+            ss << folderHtml.rdbuf();
+            string str = ss.str();
+            vector<string> links = getLinksOfFolder(url, str);
+            for (string &link : links)
+                this->addToQueue(link, path + domain+"_"+fileName + "/");
         }
-        return status;
+        return true;
     }
 
+    void addToQueue(string url, string path = "./") {
+        downloadQueue.push({url, path});
+    }
+
+    bool processQueue() {
+        int status = false;
+        while (!downloadQueue.empty()) {
+            status = download(downloadQueue.front().first,
+                              downloadQueue.front().second);
+            downloadQueue.pop();
+        }
+        return status;
+    };
 
   private:
     int sockfd;
     string buffer;
+    queue<pair<string, string>> downloadQueue;
 };
 
-int main() {
-    Socket s;
-    cout << boolalpha << s.connect("www.example.com");
-    s.download("www.example.com/index.html");
+int main(int argc, char *argv[]) {
+    vector<Socket> sockets;
+    /* string domain, filePath, fileName; */
+    /* linkType type; */
+    if (argc < 2) {
+        cout << "Usage: " << argv[0] << " [url1] [url2]..\n";
+        return 0;
+    } else {
+        for (int i = 1; i < argc; i++) {
+            sockets.push_back(Socket());
+            sockets.back().addToQueue(argv[i]);
+            /* int j; */
+            /* addressProcess(argv[i], domain, filePath, fileName, type); */
+            /* for (j = 0; j < sockets.size() && sockets[j].domain != domain; j++) */
+            /*     ; */
+            /* if (j == sockets.size()) { */
+            /*     sockets.push_back(Socket(domain)); */
+            /* } */
+            /* sockets[j].addToQueue(argv[i]); */
+        }
+    }
+    vector<future<bool>> socketStatus;
+    for (Socket &s : sockets)
+        socketStatus.push_back(async(launch::async, &Socket::processQueue, &s));
 }
